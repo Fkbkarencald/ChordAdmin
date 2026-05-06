09ba75de7f38d99bb4adda3473f3f5e48ae19f34
Frankie Benjamin <fkbcald@gmail.com>
2026-05-06 13:23:03 +1000

    Initial Commit

---
+++ ChordAdmin/ChordAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordAdminApp.swift
+//  ChordAdmin
+//
+//  Created by Frankie Benjamin on 6/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChordAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChordAdmin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChordAdmin
+//
+//  Created by Frankie Benjamin on 6/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
